5de7f55936d12abec0ea4bb52fd2415d3c5b8fcf
astro <astro@LAPTOP-IJ2I8OQJ>
2020-10-30 18:12:26 +0800

    test

--- RoboRM/src/main/java/com/roborm/controller/FinInstController.java
+++ RoboRM/src/main/java/com/roborm/controller/FinInstController.java
@@ -2,11 +2,18 @@ package com.roborm.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.roborm.exception.ResourceNotFoundException;
 import com.roborm.model.FinInst;
 import com.roborm.repository.FinInstRepository;
 
@@ -21,4 +28,26 @@ public class FinInstController {
     public List<FinInst> getAllFinInst() {
         return finInstRepository.findAll();
     }
+	
+	@GetMapping("/fininsts/{id}")
+    public ResponseEntity<FinInst> getFinInstById(@PathVariable(value = "id") Long finInstId) 
+    	throws ResourceNotFoundException{
+		FinInst fi = finInstRepository.findById(finInstId)
+		          .orElseThrow(() -> new ResourceNotFoundException("FI not found for this id :: " + finInstId));
+		        return ResponseEntity.ok().body(fi);
+    }
+}
+
+class PostFinInst
+{
+	private long fi_id;
+
+	public long getFi_id() {
+		return fi_id;
+	}
+
+	public void setFi_id(long fi_id) {
+		this.fi_id = fi_id;
+	}
+	
 }
