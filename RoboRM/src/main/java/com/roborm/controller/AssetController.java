1c61d069c72d83779978a2289b69ff600d2f44be
roborm <73687797+roborm@users.noreply.github.com>
2020-10-31 00:11:31 +0800

    Asset

--- RoboRM/src/main/java/com/roborm/controller/AssetController.java
+++ RoboRM/src/main/java/com/roborm/controller/AssetController.java
@@ -2,11 +2,20 @@ package com.roborm.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.roborm.exception.ResourceNotFoundException;
+import com.roborm.manager.AssetManager;
 import com.roborm.model.Asset;
 import com.roborm.repository.AssetRepository;
 
@@ -20,5 +29,26 @@ public class AssetController {
 	public List<Asset> getAllAsset() {
 		return assetRepository.findAll();
 	}
+	
+	@GetMapping("/assets/{id}")
+    public ResponseEntity<Asset> getFinInstById(@PathVariable(value = "id") Long assetId) 
+    	throws ResourceNotFoundException{
+		Asset asset = assetRepository.findById(assetId)
+		          .orElseThrow(() -> new ResourceNotFoundException("Asset not found for this id : " + assetId));
+		        return ResponseEntity.ok().body(asset);
+    }
+	
+	@PostMapping("/add")
+    public Asset createAsset(@Valid @RequestBody Asset asset) {
+        return assetRepository.save(asset);
+    }
+	
+	@PutMapping("/update")
+    public ResponseEntity<Asset> updateAsserUserIdById(@Valid @RequestBody Asset newAsset) throws ResourceNotFoundException {
+		AssetManager asset = new AssetManager(assetRepository);
+		return ResponseEntity.ok().body(asset.UpdateUserIdById(newAsset.getAssetId(), newAsset));
+	}
+	
+	
 
 }
---
+++ RoboRM/src/main/java/com/roborm/manager/AssetManager.java
@@ -0,0 +1,42 @@
+package com.roborm.manager;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.roborm.exception.ResourceNotFoundException;
+import com.roborm.model.Asset;
+import com.roborm.repository.AssetRepository;
+
+
+@Service
+public class AssetManager {
+	private AssetRepository assetRepository;
+	
+	public AssetManager(AssetRepository assetRepository)
+	{
+		this.assetRepository = assetRepository;
+	}
+	
+	public List<Asset> findAll()
+	{
+		return assetRepository.findAll();
+	}
+	
+	public Long findById(Long id) throws ResourceNotFoundException
+	{
+		Asset asset = assetRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Asset User Id not found for this id :: " + id));
+		return asset.getUserId();
+	}
+	
+	public Asset UpdateUserIdById(Long id, Asset newAsset) throws ResourceNotFoundException
+	{
+		Asset asset = assetRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + id));
+		asset.setUserId(newAsset.getUserId());
+		asset.setAmt(newAsset.getAmt());
+
+		final Asset updateAsset = assetRepository.save(asset);
+		return updateAsset;
+	}
+	
+}
