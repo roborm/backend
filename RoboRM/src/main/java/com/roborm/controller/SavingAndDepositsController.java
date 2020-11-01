2b95f01a81271ee3b12d312113a1680e709e6cc4
Yi Long <ongyl88@gmail.com>
2020-11-01 17:57:59 +0800

    add sad

--- RoboRM/src/main/java/com/roborm/controller/SavingAndDepositsController.java
+++ RoboRM/src/main/java/com/roborm/controller/SavingAndDepositsController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,13 @@ public class SavingAndDepositsController {
 	{
 		return ResponseEntity.ok().body(savingAndDepositsRepository.findByUserId(sad.getUserId()));
 	}
+	
+	@PostMapping("/add")
+	public SavingAndDeposits addSAD (@Valid @RequestBody SavingAndDeposits sad)
+	{
+		return savingAndDepositsRepository.save(sad);
+	}
+
 	
 	@PutMapping("/update")
 	public ResponseEntity<SavingAndDeposits> updateSADByID(@Valid @RequestBody SavingAndDeposits sad) throws ResourceNotFoundException
